131140ca049b8903144d218512ce5d4802e8ed9b
Oreo <a2369355333@yahoo.com.tw>
2026-01-30 02:28:20 +0800

    Fix: downgrade to Next.js 14 and replace fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 import MyQueryClient from "@/providers/my-query-client";
 import Snow from "@/component/snow";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const robotoMono = Roboto_Mono({
+  variable: "--font-roboto-mono",
   subsets: ["latin"],
 });
 
@@ -26,9 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${inter.variable} ${robotoMono.variable} antialiased`}>
         <Snow />
         <MyQueryClient>{children}</MyQueryClient>
       </body>
